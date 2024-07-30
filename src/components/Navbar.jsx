c95a5db3acb379ac27de4251b90144a2df08b708
AmaanKhan <amaankhan61697@gmail.com>
2024-07-30 15:57:08 +0530

    addedName

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import SearchBar from "./SearchBar";
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <span className="Name">Amaan's Web Application</span>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
